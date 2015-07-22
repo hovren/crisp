cbd4fc575a21a94acf35e450d24b6a7e5f8e07db
Hannes Ovrén <hannes.ovren@liu.se>
2015-07-22 15:54:43 +0200

    Update module __init__. This breaks backwards compatibility.

--- crisp/__init__.py
+++ crisp/__init__.py
@@ -3,33 +3,17 @@
 ========================================
 Camera-to-IMU calibration toolbox
 ========================================
-This package solves two tasks: finding the relative pose between a camera and IMU
-(gyroscope), and finding the timesynchronization for the same.
-   
-Relative pose estimation
-========================
-   estimate_pose
-   
-Time synchronization
-========================
-    sync_camera_gyro
-    sync_camera_gyro_manual
-    refine_time_offset
+This package solves the task of finding the parameters
+that relate gyroscope data with video data.
 
-Utilities
-========================    
-    good_sequences_to_track
-    IMU
-    Camera
+To run, please see the README or the class AutoCalibrator.
 """
 
 __author__ = "Hannes Ovrén"
-__copyright__ = "Copyright 2013, Hannes Ovrén"
+__copyright__ = "Copyright 2015, Hannes Ovrén"
 __license__ = "GPL"
 __email__ = "hannes.ovren@liu.se"
 
-from .imu import IMU
-from .camera import Camera
-from .timesync import sync_camera_gyro, sync_camera_gyro_manual, refine_time_offset, good_sequences_to_track
-from .pose import estimate_pose
-from .fastintegrate import integrate_gyro_quaternion_uniform
+from camera import CameraModel, AtanCameraModel
+from stream import GyroStream, VideoStream, OpenCvVideoStream
+from calibration import AutoCalibrator, CalibrationError, InitializationError
