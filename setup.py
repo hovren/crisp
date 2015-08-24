9f61d798ebb467e73efb9522980d0f95a078ddd9
Hannes Ovrén <hannes.ovren@liu.se>
2015-08-24 09:50:20 +0200

    Improved packaging
    Add parts missing to allow upload to PyPI.
    Installation requires numpy to be previously installed.

--- setup.py
+++ setup.py
@@ -1,20 +1,74 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+#from distutils.core import setup
+#from distutils.extension import Extension
+#from setuptools.command.sdist import sdist as _sdist
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+import sys
+
+try:
+    import numpy as np
+except ImportError:
+    print "Please install numpy before building this package"
+    raise
+
+try:
+    #from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
+        
 
 # Fast quaternion integration module
-fastint_sources = ["crisp/fastintegrate/fastint.pyx",]
-fastint_module = Extension("crisp.fastintegrate", fastint_sources)
+file_ext = 'pyx' if USE_CYTHON else 'c'
+fastint_sources = ["crisp/fastintegrate/fastintegrate.{}".format(file_ext),]
+
+ext_modules = [Extension("crisp.fastintegrate", fastint_sources, include_dirs=[np.get_include()]),]
+
+if USE_CYTHON:
+    ext_modules = cythonize(ext_modules)
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: GNU General Public License (GPL)',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2.7'
+]
+
+keywords = 'gyroscope gyro camera imu calibration synchronization'
+
+requires = [ 'cv2', # OpenCV
+             'numpy',
+             'scipy',
+             'matplotlib'
+]
+
+long_description = open('README.md','r').read()
 
 setup(name='crisp',
-      version='2.0',
+      version='2.1rc12',
       author="Hannes Ovrén",
       author_email="hannes.ovren@liu.se",
+      url="https://github.com/hovren/crisp",
       description="Camera-to-IMU calibration and synchronization toolkit",
+      long_description=long_description,
       license="GPL",
       packages=['crisp'],
-      ext_modules=[fastint_module],
-      cmdclass={'build_ext' : build_ext},
+      ext_modules=ext_modules,
+#      cmdclass={'build_ext' : build_ext},
+      classifiers=classifiers,
+      install_requires=["numpy"],
+      requires=requires,
+      keywords=keywords,
+#      cmdclass={'build_ext' : build_ext}
       )
