1d32b6677547262dc7ea797a3b971d3189a38889
Hannes Ovrén <hannes.ovren@liu.se>
2015-08-24 10:14:46 +0200

    Change version to 2.1

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ requires = [ 'cv2', # OpenCV
 
 
 setup(name='crisp',
-      version='2.1rc13',
+      version='2.1',
       author="Hannes Ovrén",
       author_email="hannes.ovren@liu.se",
       url="https://github.com/hovren/crisp",
