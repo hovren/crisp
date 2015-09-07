7a84b59bac85d4b05f096eadab0a36c4f13a209c
Hannes Ovrén <hannes.ovren@liu.se>
2015-09-07 17:38:21 +0200

    py3k: Support OpenCV 3.0
    Apparently the cv2.cv module does not exist anymore in OpenCV3.

--- crisp/stream.py
+++ crisp/stream.py
@@ -19,6 +19,12 @@ import numpy as np
 
 from . import fastintegrate, tracking
 
+# Handle OpenCV 2.4.x -> 3.0
+try:
+    CV_CAP_PROP_POS_MSEC = cv2.cv.CV_CAP_PROP_POS_MSEC
+except AttributeError:
+    CV_CAP_PROP_POS_MSEC = cv2.CAP_PROP_POS_MSEC
+
 class GyroStream(object):
     def __init__(self):
         self.__last_dt = None
@@ -251,9 +257,9 @@ class OpenCvVideoStream(VideoStream):
         t2 = t + self.duration*1000.0 if self.duration is not None else None
 
         for i in range(2): # Sometimes needed for setting to stick
-            vc.set(cv2.cv.CV_CAP_PROP_POS_MSEC, t)
+            vc.set(CV_CAP_PROP_POS_MSEC, t)
             vc.read()
-        t = vc.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
+        t = vc.get(CV_CAP_PROP_POS_MSEC)
         counter = 0
         retval = True
         while retval and (t2 is None or (t2 is not None and t < t2)):
@@ -265,5 +271,5 @@ class OpenCvVideoStream(VideoStream):
                 raise IOError("Failed to get frame at time %.2f" % t)
             else:
                 pass # Loop will end normally
-            t = vc.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
+            t = vc.get(CV_CAP_PROP_POS_MSEC)
             counter += 1
